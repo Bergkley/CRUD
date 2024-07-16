e6c45982d5d1a361b3784ac78f681bc1a8ad3bf1
Bergkley <bergkley@gmail.com>
2024-07-15 21:48:53 -0300

    feat: configurando o projeto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
-import "reflect-metadata";
-import express from "express";
+import "reflect-metadata"
+import express from "express"
 
+import {routes} from "./routes";
+import "./database";
 
 const app = express();
+app.use(express.json())
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.use(routes)
+
+app.listen(3000, ()=> console.log("Server is running"))
