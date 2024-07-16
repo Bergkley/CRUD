93a4b26140936b15b86498340f1e3dd08b49d5cd
Bergkley <bergkley@gmail.com>
2024-07-16 20:36:58 -0300

    feat: criando class Category

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -1,3 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
+export class Category {
+    
+    id: string
+
+    
+    name: string
+
+    
+    description: string
+
+    
+    created_at: Date
+
+    
+}
