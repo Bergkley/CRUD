3de8dd9946dc8e8ce7394e7dcb2a681886ea803e
Bergkley <bergkley@gmail.com>
2024-07-16 20:55:22 -0300

    feat: criando type CategoryRequest

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -6,22 +6,3 @@ type CategoryRequest = {
     description: string;
 }
 
-export class CreateCategoryService {
-    async execute({name,description}:CategoryRequest): Promise<Category | Error> {
-
-        const repo = getRepository(Category);
-
-        if(await repo.findOne({name})) {
-            throw new Error("Category already exists");
-        }
-
-        const category = repo.create({
-            name,
-            description
-        });
-
-        await repo.save(category);
-
-        return category;
-    }
-}
