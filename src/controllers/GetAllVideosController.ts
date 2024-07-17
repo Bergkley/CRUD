17d17c08218828e20f335e1ae9a3854d3802bd17
Bergkley <bergkley@gmail.com>
2024-07-17 20:26:20 -0300

    feat: importando

--- src/controllers/GetAllVideosController.ts
+++ src/controllers/GetAllVideosController.ts
@@ -1,12 +1,3 @@
 import { response } from "express";
 import { GetAllCategoriesService } from "../services/GetAllCategoriesService";
 
-export class GetAllVideosController {
-    async handle() {
-        const service = new GetAllCategoriesService();
-
-        const videos = await service.execute()
-
-        return response.json(videos)
-    }
-}
