8fc45968ce5d59bd7b60556293ec97d778cedf37
Bergkley <bergkley@gmail.com>
2024-07-16 21:38:06 -0300

    feat: criando routa para deletar categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
+routes.delete("/categories/:id", new DeleteCategoryController().handle);
 
 export { routes }
