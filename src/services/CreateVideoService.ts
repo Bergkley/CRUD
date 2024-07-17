1e57567687354b2e8f1e9fe3cf73fcd6782676db
Bergkley <bergkley@gmail.com>
2024-07-17 20:16:00 -0300

    feat: criando class CreateVideosService

--- src/services/CreateVideoService.ts
+++ src/services/CreateVideoService.ts
@@ -10,3 +10,19 @@ type VideoRequest = {
     category_id: string;
 }
 
+class CreateVideosService{
+    async execute({name,description,duration,category_id}:VideoRequest):Promise<Error | Videos>{
+        const repo = getRepository(Videos)
+        const repoCategory = getRepository(Category);
+
+        if (!await repoCategory.findOne(category_id)){
+            return new Error("Category does not exists!")
+        }
+        const video = repo.create({name, description, duration, category_id})
+        await repo.save(video);
+
+        return video
+    }
+}
+
+export {CreateVideosService}
